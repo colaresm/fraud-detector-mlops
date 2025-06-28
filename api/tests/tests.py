3761636b02dc37eca3c20fd7b6fbbe6d5bb16475
Marcelo Colares <marcelocolares@MacBook-Air-de-Marcelo.local>
2025-06-28 18:29:33 -0300

    Insert test of get risk

--- api/tests/tests.py
+++ api/tests/tests.py
@@ -1,8 +1,8 @@
 import unittest
 import numpy as np
 from unittest.mock import patch, MagicMock
-from api.utils import utils
-from api.services import app ,services
+from api.services import app
+from services import get_risk
 
 class TestSoma(unittest.TestCase):
     def test_soma_positiva(self):
@@ -13,6 +13,35 @@ class TestSoma(unittest.TestCase):
     
     def test_is_number(self):
         self.assertIsInstance(app.soma(-1, -1), (int, float))
+
+
+class TestGetRisk(unittest.TestCase):
+    @patch('services.load_model_and_scaler')
+    @patch('services.get_params_by_prediction')
+    def test_get_risk_returns_expected_prediction(self, mock_get_params, mock_load_model_and_scaler):
+        mock_get_params.return_value = [5000, 700, 1000, 2]
+
+        mock_scaler = MagicMock()
+        mock_scaler.transform.return_value = [[0.5, 0.7, 0.4, 0.1]]
+
+        mock_model = MagicMock()
+        mock_model.predict.return_value = [2]
+
+        mock_load_model_and_scaler.return_value = (mock_model, mock_scaler)
+
+        input_data = {
+            "monthly_income": 5000,
+            "credit_score": 700,
+            "current_debt": 1000,
+            "late_payments": 2
+        }
+
+        result = get_risk(input_data)
+
+        mock_get_params.assert_called_once_with(input_data)
+        mock_scaler.transform.assert_called_once_with([[mock_get_params.return_value]])
+        mock_model.predict.assert_called_once_with([[0.5, 0.7, 0.4, 0.1]])
+        self.assertEqual(result, [2])
 
 
 if __name__ == "__main__":
