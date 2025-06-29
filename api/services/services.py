4317a4efd211a398f4c5ae4ae29f1fa24beefd3c
colaresmarcelo2018 <colaresmarcelo2018@gmail.com>
2025-06-29 09:46:30 -0300

    Remove print

--- api/services/services.py
+++ api/services/services.py
@@ -5,7 +5,6 @@ import numpy as np
 def get_risk(data):    
     model, scaler_model = mlflow_server.load_model_and_scaler()
     X = np.array([utils.get_params_by_prediction(data)])
-    print(X)
     X_scaled = scaler_model.transform(X)
     prediction = model.predict(X_scaled)
     return prediction
