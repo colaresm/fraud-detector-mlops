ba3cee3f7ba1d691d75be4548400fc5c26abc717
colaresmarcelo2018 <colaresmarcelo2018@gmail.com>
2025-06-29 08:47:13 -0300

    Change name

--- api/infra/mlflow_server.py
+++ api/infra/mlflow_server.py
@@ -1,16 +1,9 @@
 import mlflow.sklearn
-import os
+import joblib
 
 def load_model_and_scaler(models_dir: str = "models"):
-    model_path = os.path.join(models_dir, "model")
-    scaler_path = os.path.join(models_dir, "scaler_model")
-
-    if not os.path.exists(model_path):
-        raise FileNotFoundError(f"Model not found at {model_path}")
-    if not os.path.exists(scaler_path):
-        raise FileNotFoundError(f"Scaler not found at {scaler_path}")
-    
-    model = mlflow.sklearn.load_model(model_path)
-    scaler = mlflow.sklearn.load_model(scaler_path)
+   
+    model = joblib.load("api/models/mlp_model_iris.pkl")
+    scaler = joblib.load("api/models/scaler_model.pkl")
     
     return model, scaler
