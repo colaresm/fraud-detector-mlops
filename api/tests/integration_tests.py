41be05176f145d7fa3b840d307c0f99c3a45f13b
colaresmarcelo2018 <colaresmarcelo2018@gmail.com>
2025-06-29 08:19:35 -0300

    Set path

--- api/tests/integration_tests.py
+++ api/tests/integration_tests.py
@@ -1,6 +1,8 @@
 import pytest
 from flask import Flask,request,jsonify
 from api.services import services
+import os
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '../../')))
 
 @pytest.fixture
 def client():
